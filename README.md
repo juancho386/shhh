835d44c6b76081c8f6f5094a1ebe7b5b5031b566
Juan <->
2024-11-08 13:42:10 +0100

    working version

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# shhh
+# SHHH
 Hibrid encryption implementation using OpenSSL CLI on Bash. (GPG-wanna-be)
+
+It is a simple bash based tool to encrypt and hash messages using Chacha20 and RSA public and private keys.
+
+## Features
+- It uses Bash so it runs in any (?) linux machine
+- ./shhs.sh has a menu. Use it
+- It is based on OpenSSL CLI
+
+
+### Principles
+- Creates keys using: openssl genpkey -algorithm RSA -out private.pem -aes256 -pkeyopt rsa_keygen_bits:16386
+- Extracts its public key using: openssl rsa -in private.pem -pubout -out public.pem
+- To encrypt a message:
+  - Generate a secure password: openssl rand 256
+  - Encrypt the message using that password over Chacha20: echo $message | openssl enc -e chacha20 -pass "pass:$password"
+  - Encrypt the password using RSA public key: echo $password|openssl rsautl -encrypt -pubin -inkey $pubkeypem
+  - Output the result as $encryptedPassword:$message
+- To decrypt a message:
+  - Get message and split by ":" to get the password and the real message
+  - Decrypt the password using the private key: echo $encPassword|openssl rsautl -decrypt -inkey shhh.private.pem
+  - Decrypt the message using it: echo $encMsg | openssl enc -d -chacha20 -pbkdf2 -pass "pass:$password"
+
+
+### Development stages
+##### Stage 1
+At the first stage, you can create your keys, share your public key with your friends and when they send you a short RSA encripted message you can copy and paste it to be decrypted.
+##### Stage 2
+The keys can be up to 16kb long to receive messages of up to 2kb long
+##### Stage 3 (current)
+Now the message is encrypted using Chacha20 with a self generated key which is encrypted using the receptor's public key. So the messages can be of any length.
+##### Stage 4
+Make it prettier using Dialog. Note: Not finished because in my current Linux distribution, dialog has a bug on --editbox that doesn't allow text bigger than ~2k long
+
+### More things to do:
+- The chacha20 password length is 256 chars. The static number might become data for hackers. Solution: Make the password length variable
+- Opt the user to output a file instead o text to be copied and pasted
+- Optionally implement zenity
+
---
+++ shhh.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $DIR
+touch ${DIR}/dummy
+
+clean_all () {
+	echo -n "" > ${DIR}/dummy
+}
+trap clean_all exit
+
+choice () {
+	local PS3="$1"
+	local oIFS=$IFS
+	local IFS=$'\t'
+	local s=($2)
+	local resp
+	select resp in ${s[@]};do 
+		if [[ "$resp" != "" ]]; then 
+			break
+		fi
+	done
+	IFS=$oIFS
+	echo $resp
+}
+
+menu () {
+	resp=$(choice "Choose: " "Write a message	Read a message	Display your public key	Create key pair	Exit")
+	if [[ "$resp" == "Write a message" ]]; then
+		write_msg
+	fi 
+	if [[ "$resp" == "Read a message" ]]; then
+		read_msg
+	fi 
+	if [[ "$resp" == "Display your public key" ]]; then
+		show_key
+	fi
+	if [[ "$resp" == "Create key pair" ]]; then
+		config
+	fi
+	if [[ "$resp" == "Exit" ]]; then
+		toExit=1
+	fi
+}
+
+write_msg () {
+	dests=$(find ./contacts -name "*.pem"|tr "\n" "\t")
+	pub_key_pem=$(choice "Whom: " "$dests")
+	read -p "Type the message: " msg
+	if [[ "$resp" != "" ]]; then 
+		local password=$(openssl rand 256|tr -d '\000\a\b\c\d\e\f\g\h\i\j\k\l\m\n\o\p\q\r\s\t\u\v\w\x\y\z"') # XXX do I need |base64?
+		#echo creando pass: $password $?
+		local encriptedMsg=$(openssl enc -e -chacha20 -pbkdf2 -pass "pass:${password}" <<<${msg}|base64 -w0)
+		#echo encriptando msg: $encriptedMsg $?
+		#test: to decrypt: openssl enc -d -chacha20 -pbkdf2 -pass "pass:${password}" <<< "${encriptedMsg}"
+		local encryptedPassword=$(openssl rsautl -encrypt -pubin -inkey $pub_key_pem 2>/dev/null <<< "$password"|base64 -w0)
+		#echo enciptando pass: $?
+		echo ${encryptedPassword}:${encriptedMsg}
+	fi
+}
+
+
+
+read_msg () {
+	echo
+	if [[ -f shhh.private.pem ]]; then
+		read -p "Type the encrypted message: " fullMsg
+		encodedPassword=$(grep -Eo "^[^:]+" <<<$fullMsg)
+		encodedMsg=$(sed -E "s/^[^:]+://g" <<< $fullMsg)
+		password=$(base64 -d <<< $encodedPassword | openssl rsautl -decrypt -inkey shhh.private.pem 2>/dev/null )
+		msg=$(base64 -d <<< $encodedMsg | openssl enc -d -chacha20 -pbkdf2 -pass "pass:${password}")
+		echo $msg
+		# sed -r "s/ //g" <<<"${encodedMsg}" | base64 -d | openssl rsautl -decrypt -inkey shhh.private.pem 2>/dev/null
+	else
+		echo "Your keys are not present. Aborting."
+	fi
+}
+
+
+show_key () {
+	if [[ -f "contacts/myself.pem" ]]; then
+		cat contacts/myself.pem
+		echo
+		echo Press any key to continue
+		read -sn1
+	else
+		echo "Your key is not present"
+	fi
+}
+
+
+config () {
+	read -sn1 -p "Your new keys are about to be created. Press C to continue or any other key to cancel" ans
+	echo
+	if [[ "${ans^^}" == "C" ]]; then
+		if [[ -f shhh.private.pem ]]; then
+			echo "This action will overwite your personal keys."
+			read -n1 -p "Are you sure you want to continue? (y/N): " ans
+		fi
+		if [[ "${ans^^}" == "Y" || ! -f shhh.private.pem ]]; then
+			echo Creating private key
+			openssl genpkey -algorithm RSA -out shhh.private.pem -aes256 -pkeyopt rsa_keygen_bits:16386
+			echo Creating public key
+			openssl rsa -in shhh.private.pem -pubout -out ./contacts/myself.pem
+		fi
+	fi
+}
+
+toExit=0
+while [[ "$toExit" == "0" ]]; do
+	menu
+done
+
